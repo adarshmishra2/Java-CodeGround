ed8fd562b542aef57bfd784d56ac93b2378f6ab7
Adarsh Mishra <31216840+adarshmishra2@users.noreply.github.com>
2018-08-17 16:18:49 +0530

    Pattern Programs

---
+++ Pattern_Program/Pattern1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Pattern1{
+ public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+		 staircase(n);
+}
+static void staircase(int n) {
+        
+         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
