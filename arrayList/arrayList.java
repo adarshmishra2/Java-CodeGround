607e2c1eb6c6af7f62768e0ec730af2dc3b5d058
Adarsh Mishra <31216840+adarshmishra2@users.noreply.github.com>
2018-08-24 12:15:13 +0530

    Add files via upload

---
+++ arrayList/arrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class arrayList{
+public static void main(String[] args){
+	List<String> ls = new ArrayList<String>();
+	ls.add("Red");
+	ls.add("yellow");
+	ls.add("blue");
+	ls.add("purple");
+	ls.add("green");
+	ls.add("white");
+	System.out.println(ls);
+}
+}
